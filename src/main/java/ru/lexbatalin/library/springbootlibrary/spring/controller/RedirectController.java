b25bc27eb0c360f95a566d6ef76502e20e297f0f
Lex <alexbtln@gmail.com>
2021-10-22 22:53:15 +0300

    some fix

--- src/main/java/ru/lexbatalin/library/springbootlibrary/spring/controller/RedirectController.java
+++ src/main/java/ru/lexbatalin/library/springbootlibrary/spring/controller/RedirectController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ru.lexbatalin.library.springbootlibrary.domain.Author;
 import ru.lexbatalin.library.springbootlibrary.spring.repository.AuthorRepository;
 
@@ -12,14 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@Controller
+@RestController
 @Log
 public class RedirectController {
 
     @Autowired
     private AuthorRepository authorRepository;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String baseUrlRedirect(HttpServletRequest request, HttpServletResponse response) {
         List<Author> authorList = authorRepository.findAll();
         System.out.println(authorList);
